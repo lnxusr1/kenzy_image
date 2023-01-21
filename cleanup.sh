9958979781f6e534e7a57f478b2cd5fc0cb41ab4
lnxusr1 <lnxusr1@kenzy.ai>
2023-01-21 12:49:32 -0500

    Adding tests to cleanup process

--- cleanup.sh
+++ cleanup.sh
@@ -2,4 +2,6 @@
 
 SCRIPT_DIR=$(CDPATH= cd -- "$(dirname -- "$0")" && pwd -P)
 find $SCRIPT_DIR/src -type d -name "__pycache__" -exec rm -rf {} +;
+find $SCRIPT_DIR/tests -type d -name "__pycache__" -exec rm -rf {} +;
 find $SCRIPT_DIR/src -name *.pyc -exec rm -rf {} \;
+find $SCRIPT_DIR/tests -name *.pyc -exec rm -rf {} \;
